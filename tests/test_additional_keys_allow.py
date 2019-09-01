5fc65ed6bc7a494afd3859330c2b7f1b41faefc1
Slawek <slawomir.hadas@yahoo.com>
2019-09-01 13:54:34 +0100

    autotests

--- tests/test_additional_keys_allow.py
+++ tests/test_additional_keys_allow.py
@@ -3,7 +3,7 @@ import unittest
 from lib.idict import Idict
 
 
-class TestAdditionalKeysIgnore(unittest.TestCase):
+class TestAdditionalKeysAllow(unittest.TestCase):
     dict1 = Idict({
         'a': None,
         'b': {
@@ -22,26 +22,29 @@ class TestAdditionalKeysIgnore(unittest.TestCase):
             },
         },
     }, {
-        "missing_keys": Idict.OPT.IGNORE
+        "missing_keys": Idict.OPT.ALLOW
     })
 
     def test_additional_keys(self):
         # non existing first element
         try:
-            TestAdditionalKeysIgnore.dict1['non_existing'] = 1
-            TestAdditionalKeysIgnore.dict1['non_existing2']['non_existing3']['non_existing4'] = 2
-            TestAdditionalKeysIgnore.dict1['bither_non_existing'] = 4
-            TestAdditionalKeysIgnore.dict1['c']['cc1']['ccc1'] = 5
+            TestAdditionalKeysAllow.dict1['non_existing'] = 1
+            TestAdditionalKeysAllow.dict1['non_existing2']['non_existing3']['non_existing4'] = 2
+            TestAdditionalKeysAllow.dict1['bither_non_existing'] = 4
+            TestAdditionalKeysAllow.dict1['c']['cc1']['ccc1'] = 5
+
         except Exception as ex:
             print(ex)
             self.assertTrue(False)
         finally:
-            obj_hash = "eydhJzogTm9uZSwgJ2InOiB7J2JiMSc6IE5vb" \
-                       "mUsICdiYjInOiBOb25lfSwgJ2MnOiB7J2NjMSc6" \
-                       "IHsnY2NjMSc6IDUsICdjY2MyJzogeydjY2NjMSc6" \
-                       "IE5vbmUsICdjJzogTm9uZSwgJ2NjMSc6IE5vbmV9LC" \
-                       "AnY2NjMyc6IEVsbGlwc2lzfX19"
-            self.assertEqual(TestAdditionalKeysIgnore.dict1.hash_code(), obj_hash)
+            obj_hash = "eydhJzogTm9uZSwgJ2InOiB7J2JiMSc6IE5vbmUsICdiY" \
+                   "jInOiBOb25lfSwgJ2MnOiB7J2NjMSc6IHsnY2NjMSc6IDUs" \
+                   "ICdjY2MyJzogeydjY2NjMSc6IE5vbmUsICdjJzogTm9uZSwgJ" \
+                   "2NjMSc6IE5vbmV9LCAnY2NjMyc6IEVsbGlwc2lzfX0sICdub25" \
+                   "fZXhpc3RpbmcnOiAxLCAnbm9uX2V4aXN0aW5nMic6IHsnbm9uX" \
+                   "2V4aXN0aW5nMyc6IHsnbm9uX2V4aXN0aW5nNCc6IDJ9fSwgJ2" \
+                   "JpdGhlcl9ub25fZXhpc3RpbmcnOiA0fQ=="
+            self.assertEqual(TestAdditionalKeysAllow.dict1.hash_code(), obj_hash)
 
 
 if __name__ == '__main__':
--- tests/test_additional_keys_ignore.py
+++ tests/test_additional_keys_ignore.py
@@ -3,7 +3,7 @@ import unittest
 from lib.idict import Idict
 
 
-class TestAdditionalKeysAllow(unittest.TestCase):
+class TestAdditionalKeysIgnore(unittest.TestCase):
     dict1 = Idict({
         'a': None,
         'b': {
@@ -22,29 +22,29 @@ class TestAdditionalKeysAllow(unittest.TestCase):
             },
         },
     }, {
-        "missing_keys": Idict.OPT.ALLOW
+        "missing_keys": Idict.OPT.IGNORE
     })
 
     def test_additional_keys(self):
         # non existing first element
         try:
-            TestAdditionalKeysAllow.dict1['non_existing'] = 1
-            TestAdditionalKeysAllow.dict1['non_existing2']['non_existing3']['non_existing4'] = 2
-            TestAdditionalKeysAllow.dict1['bither_non_existing'] = 4
-            TestAdditionalKeysAllow.dict1['c']['cc1']['ccc1'] = 5
+            TestAdditionalKeysIgnore.dict1['non_existing'] = 1
+            TestAdditionalKeysIgnore.dict1['non_existing2']['non_existing3']['non_existing4'] = 2
+            TestAdditionalKeysIgnore.dict1['bither_non_existing'] = 4
+            TestAdditionalKeysIgnore.dict1['c']['cc1']['ccc1'] = 5
 
+            print(repr(TestAdditionalKeysIgnore.dict1))
         except Exception as ex:
             print(ex)
+            print("EEEEEEEEEEEEEEEEEEEEE")
             self.assertTrue(False)
         finally:
-            obj_hash = "eydhJzogTm9uZSwgJ2InOiB7J2JiMSc6IE5vbmUsICdiY" \
-                   "jInOiBOb25lfSwgJ2MnOiB7J2NjMSc6IHsnY2NjMSc6IDUs" \
-                   "ICdjY2MyJzogeydjY2NjMSc6IE5vbmUsICdjJzogTm9uZSwgJ" \
-                   "2NjMSc6IE5vbmV9LCAnY2NjMyc6IEVsbGlwc2lzfX0sICdub25" \
-                   "fZXhpc3RpbmcnOiAxLCAnbm9uX2V4aXN0aW5nMic6IHsnbm9uX" \
-                   "2V4aXN0aW5nMyc6IHsnbm9uX2V4aXN0aW5nNCc6IDJ9fSwgJ2" \
-                   "JpdGhlcl9ub25fZXhpc3RpbmcnOiA0fQ=="
-            self.assertEqual(TestAdditionalKeysAllow.dict1.hash_code(), obj_hash)
+            obj_hash = "eydhJzogTm9uZSwgJ2InOiB7J2JiMSc6IE5vb" \
+                       "mUsICdiYjInOiBOb25lfSwgJ2MnOiB7J2NjMSc6" \
+                       "IHsnY2NjMSc6IDUsICdjY2MyJzogeydjY2NjMSc6" \
+                       "IE5vbmUsICdjJzogTm9uZSwgJ2NjMSc6IE5vbmV9LC" \
+                       "AnY2NjMyc6IEVsbGlwc2lzfX19"
+            self.assertEqual(TestAdditionalKeysIgnore.dict1.hash_code(), obj_hash)
 
 
 if __name__ == '__main__':
